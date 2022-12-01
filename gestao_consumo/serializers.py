67ddcacbd303f1d61b12e99e7f2c16848ca906a5
Jonas Castro <jonascastro.dev@gmail.com>
2022-12-01 16:09:04 -0300

    Primeiro commit, configurando o projeto

---
+++ gestao_consumo/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    username = serializers.CharField(write_only=True, required=False)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'first_name', 'is_active']
+    
+    
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        validated_data['is_active'] = False
+        validated_data['username'] = validated_data['email']
+
+        return super(UserCreateSerializer, self).create(validated_data)

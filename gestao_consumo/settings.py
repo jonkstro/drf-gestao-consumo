4ff3b21bf760a2e50ee316513bcf57d2a8e79e88
Jonas Castro <jonascastro.dev@gmail.com>
2022-12-22 15:38:58 -0300

    Fix error

--- gestao_consumo/settings.py
+++ gestao_consumo/settings.py
@@ -133,7 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# STATIC URL DO RAILWAY:
+STATIC_URL = '/static/rest_framework/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

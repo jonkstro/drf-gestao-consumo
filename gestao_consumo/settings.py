b5956c4891534c40d642108870cd317a5fb2fdfb
Jonas Castro <jonascastro.dev@gmail.com>
2022-12-22 16:18:22 -0300

    fixed crash

--- gestao_consumo/settings.py
+++ gestao_consumo/settings.py
@@ -134,14 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
-# STATIC URL DO RAILWAY:
-STATIC_ROOT = BASE_DIR + '/static/'
 STATIC_URL = '/static/'
+# STATIC URL DO RAILWAY:
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 
